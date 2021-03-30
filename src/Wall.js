2305074c93da5437f8d2fc6af4146905c2dc5e0b
Abhishek <mallepalliabhishek@gmail.com>
2021-03-29 23:39:18 -0700

    componentized the material, which should improve performance

--- src/Wall.js
+++ src/Wall.js
@@ -5,6 +5,7 @@ import {useThree} from 'react-three-fiber'
 import { useGLTF, useTexture } from '@react-three/drei'
 import {DoubleSide, Matrix4, VideoTexture} from 'three'
 import {  MeshStandardMaterial, PlaneGeometry, TextureLoader, Euler, LinearFilter, RGBFormat } from 'three';
+import Video from './Video'
 export default function Wall(props) {
   const { viewport } = useThree();
   const group = useRef()
@@ -13,35 +14,35 @@ export default function Wall(props) {
   // const material = new MeshStandardMaterial({
   //     normalMap:texture
   // })
-  const video = useMemo(() => {
-    const vid = document.createElement("video");
-    vid.src = 'miscvideo.mov';
-    vid.crossOrigin = "anonymous";
-    vid.play();
-    vid.volume = 0.005
-    return vid;
-  }, []);
-  const texture = useMemo(() => {
-    const t = new VideoTexture(video);
-    t.flipY = false;
-    t.minFilter = LinearFilter;
-    t.magFilter = LinearFilter;
-    t.format = RGBFormat;
-    return t;
-  }, [video]);
+  // const video = useMemo(() => {
+  //   const vid = document.createElement("video");
+  //   vid.src = 'miscvideo.mov';
+  //   vid.crossOrigin = "anonymous";
+  //   vid.play();
+  //   vid.volume = 0.005
+  //   return vid;
+  // }, []);
+  // const texture = useMemo(() => {
+  //   const t = new VideoTexture(video);
+  //   t.flipY = false;
+  //   t.minFilter = LinearFilter;
+  //   t.magFilter = LinearFilter;
+  //   t.format = RGBFormat;
+  //   return t;
+  // }, [video]);
 
-  const floor = useRef();
+  const plane = useRef();
   let euler = new Euler(...props.orientation)
   let transformation = new Matrix4;
   transformation.makeRotationFromEuler(euler)
   useEffect(()=>{
-    floor.current.applyMatrix4(transformation)
-  },[floor])
+    plane.current.applyMatrix4(transformation)
+  },[plane])
 
   return (
     <mesh position={props.position}>
-        <planeBufferGeometry ref={floor} attach={'geometry'} args={props.size} />
-        <meshBasicMaterial map={texture} skinning={true} side={DoubleSide}/>
+        <planeBufferGeometry ref={plane} attach={'geometry'} args={props.size} />
+        <Video />
         
     </mesh>
   )
